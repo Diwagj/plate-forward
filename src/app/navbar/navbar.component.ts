c834535d1a5e65364ab7d603510ee8a4ce19dbd9
Ace <20239150@s.ubaguio.edu>
2026-04-22 15:31:49 +0800

    feat: add user profile functionality to navbar with login state management

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UserServiceService } from '../user-service.service';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  private userService = inject(UserServiceService);
+  private subscription: Subscription = new Subscription();
+  isLoggedInFlag = false;
 
+  ngOnInit() {
+    this.subscription = this.userService.currentUser$.subscribe(user => {
+      this.isLoggedInFlag = !!user;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  isLoggedIn() {
+    return this.isLoggedInFlag;
+  }
+
+  logout() {
+    this.userService.logout();
+  }
 }
